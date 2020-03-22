a952e1f712fe37b8cfbad89e366cde50e7d9bbf5
Lija Alex <48295613+LijaAlex12@users.noreply.github.com>
2020-03-22 19:46:15 +0530

    basics1

---
+++ basics1.js
@@ -0,0 +1,42 @@
+// manipulating text
+$('h1').addClass("big-title margin-50");
+ $('button').text("ncjf")
+ $('button').html("<em>hey</em>");
+
+// manipulating attributes
+getting the value of the attribute
+console.log($("img").attr("src"));
+ $("a").attr("href","https://yahoo.com");
+
+//adding click event listeners to html elements using jquery
+ $('h1').click(function(){ //click is a listener
+   $('h1').css("color","purple");
+ });
+//
+ $('button').click(function(){
+ $('h1').css('color','purple');
+ });
+
+//adding keypress event listeners
+ $(document).keypress(function(event){
+  console.log(event.key);
+ });
+
+ $('input').keypress(function(event){
+   console.log(event.key);
+ });
+
+ $(document).keypress(function(event){
+   $('h1').text(event.key);
+ });
+
+// on event listeners
+ $(document).on("mouseover",function(){
+   $('h1').css('color','red');
+ });
+
+ $('h1').before("<button>new</button>");
+ $('h1').after("<button>new</button>");
+ $('h1').prepend("<button>new</button>");
+ $('h1').append("<button>new</button>");
+ $('button').remove();
